78f425b0d81cd9dca7a1c35d35cf5998a5069949
Gustavo-Jorge-Rodrigues <gustavo.jorge.rodrigues.13@gmail.com>
2026-08-01 17:07:26 +0100

    Adição de If e Else

---
+++ src/If_Else.java
@@ -0,0 +1,32 @@
+public class If_Else {
+    public static void main(String[] args) {
+
+        // vou dar um exemplo da maneira que geralmente o if else é usado
+
+        String DiaSemana = "Segunda";
+
+        if (DiaSemana.equals("Domingo")) {
+            System.out.println("Hoje é Domingo");
+        }
+        else if (DiaSemana.equals("Segunda")) {
+            System.out.println("Hoje é Segunda");
+        }
+        else if (DiaSemana.equals("Terça")) {
+            System.out.println("Hoje é Terça");
+        }
+        else if (DiaSemana.equals("Quarta")) {
+            System.out.println("Hoje é Quarta");
+        }
+        else if (DiaSemana.equals("Quinta")) {
+            System.out.println("Hoje é Quinta");
+        }
+        else if (DiaSemana.equals("Sexta")) {
+            System.out.println("Hoje é Sexta");
+        }
+        else if (DiaSemana.equals("Sábado")) {
+            System.out.println("Hoje é Sábado");
+        }
+
+
+        }
+    }
