6253ce4babc335b47238d63bacc1e550d002476e
Gustavo-Jorge-Rodrigues <gustavo.jorge.rodrigues.13@gmail.com>
2026-08-01 12:29:27 +0100

    Adição das variáveis

---
+++ src/Variáveis.java
@@ -0,0 +1,20 @@
+public class Variáveis {
+    public static void main(String[] args) {
+
+
+    // exemplo das variáveis mais usadas👍
+
+        int ExemploInt = 10;
+            // int é usado para números inteiros
+        double ExemploDouble = 1.753870918347091836409138;
+            // double é para números quebrados ele é mais preciso e armazena mais dados
+        float ExemploFloat = 1.75812938461764218942613f;
+            // float é pra numeros quebraos mas é menos preciso, armazena menos dados e precisa o f
+        char ExemploChar = 'A';
+            // char é usado para letras
+        boolean ExemploBoolean = false;
+            // Boolean é pra dizer se é verdadeiro ou falso
+        String ExmploString = "Palavra ou frase";
+            // String é usado para palavra ou frase
+    }
+}
