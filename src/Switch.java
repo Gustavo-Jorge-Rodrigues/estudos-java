eeee057dd190750383bf305f922fa8834b9678a4
Gustavo-Jorge-Rodrigues <gustavo.jorge.rodrigues.13@gmail.com>
2026-08-02 19:54:58 +0100

    Adição de switch

---
+++ src/Switch.java
@@ -0,0 +1,40 @@
+public class Switch {
+    public static void main(String[] args) {
+
+        // vou fazer o mesmo sistema que fiz com if else só que com switch
+
+        String ExemploSwitch = "Domingo";
+
+        switch (ExemploSwitch) {
+
+            case "Domingo":
+                System.out.println("Hoje é Domingo");
+                break;
+            case "Segunda":
+                System.out.println("Hoje é Segunda");
+                break;
+            case "Terça":
+                System.out.println("Hoje é Terça");
+                break;
+            case "Quarta":
+                System.out.println("Hoje é Quarta");
+                break;
+            case "Quinta":
+                System.out.println("Hoje é Quinta");
+                break;
+            case "Sexta":
+                System.out.println("Hoje é Sexta");
+                break;
+            case "Sábado":
+                System.out.println("Hoje é Sábado");
+                break;
+
+
+        }
+
+
+
+
+
+    }
+}
